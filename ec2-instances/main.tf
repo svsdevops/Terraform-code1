241da1c087a9bcac5305c5ea5c2061c237379516
root <root@ip-172-31-0-22.ap-south-1.compute.internal>
2023-12-20 12:12:38 +0000

    pushing changes to main.tf

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -2,12 +2,20 @@ provider "aws" {
   region = "ap-south-1"  # Replace with your desired AWS region
 }
 
+variable "instance_name" {
+   type = list(string)
+   default = ["prometheus-server", "prometheus-agent"}
+}
+
+
 resource "aws_instance" "example_instance" {
+  count = length(var.instance_name)
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (replace with your preferred AMI)
   instance_type = "t2.micro"               # Instance type (adjust as needed)
+  key_name = "Jenkins"
 
   tags = {
-    Name = "prometheus-server"
+    Name = var.instance_name[count.index]
   }
 }
 
